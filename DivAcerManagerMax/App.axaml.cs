0c8e873f0198952f1885f4e5fcb4794235e0807b
Divyansh <Divyanshtiwari2005@gmail.com>
2025-05-12 00:15:09 +0530

    Forced #1B89D8 (Blue) Accent color for a consistent look across devices

--- DivAcerManagerMax/App.axaml.cs
+++ DivAcerManagerMax/App.axaml.cs
@@ -1,22 +1,29 @@
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Themes.Fluent;
 
 namespace DivAcerManagerMax;
 
-public partial class App : Application
+public class App : Application
 {
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+
+        var accentColor = Color.Parse("#1B89D8");
+        Application.Current.Resources["SystemAccentColor"] = accentColor;
+        Application.Current.Resources["SystemAccentBrush"] = new SolidColorBrush(accentColor);
+
     }
 
     public override void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-        {
             desktop.MainWindow = new MainWindow();
-        }
+
 
         base.OnFrameworkInitializationCompleted();
     }
