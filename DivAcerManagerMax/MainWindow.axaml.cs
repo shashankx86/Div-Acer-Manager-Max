b2802ddec06e6239b7d2f35de0d399fb64722cdb
Divyansh <Divyanshtiwari2005@gmail.com>
2025-07-01 15:54:45 +0530

    v0.9.1

--- DivAcerManagerMax/MainWindow.axaml.cs
+++ DivAcerManagerMax/MainWindow.axaml.cs
@@ -17,7 +17,7 @@ namespace DivAcerManagerMax;
 public partial class MainWindow : Window, INotifyPropertyChanged
 {
     private readonly string _effectColor = "#0078D7";
-    private readonly string ProjectVersion = "0.9.0";
+    private readonly string ProjectVersion = "0.9.1";
 
     // UI Controls (will be bound via NameScope)
     private Button _applyKeyboardColorsButton;
